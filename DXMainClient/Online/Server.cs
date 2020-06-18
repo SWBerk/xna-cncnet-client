9e4ea8abe1ac883f763ee71726be14f90f5eb52a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2020-06-18 11:21:54 +0200

    Enable explicit specification of CnCNet Server SSL/TLS protocols

--- DXMainClient/Online/Server.cs
+++ DXMainClient/Online/Server.cs
@@ -1,21 +1,26 @@
-﻿namespace DTAClient.Online
+﻿using System.Security.Authentication;
+
+namespace DTAClient.Online
 {
     /// <summary>
     /// A struct containing information on an IRC server.
     /// </summary>
     public struct Server
     {
-        public Server(string host, string name, int[] ports, bool useSsl = false)
+        public Server(string host, string name, int[] ports, bool useSsl = false,
+            SslProtocols? sslProtocols = null)
         {
             Host = host;
             Name = name;
             Ports = ports;
             UseSsl = useSsl;
+            SslProtocols = sslProtocols;
         }
 
         public string Host;
         public string Name;
         public int[] Ports;
         public bool UseSsl { get; }
+        public SslProtocols? SslProtocols { get; set; }
     }
 }
